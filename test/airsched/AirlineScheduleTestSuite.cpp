324ea2bf2e99eb8eeeffeaf74086e04c0acc11e1
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-02-12 22:32:43 +0100

    [AirSched][Test] Migrated to Boost.UTF (from ExtraCC/CPPUnit).

--- test/airsched/AirlineScheduleTestSuite.cpp
+++ test/airsched/AirlineScheduleTestSuite.cpp
@@ -1,137 +1,149 @@
+/*!
+ * \page AirlineScheduleTestSuite_cpp Command-Line Test to Demonstrate How To Test the AirSched Project
+ * \code
+ */
+// //////////////////////////////////////////////////////////////////////
+// Import section
+// //////////////////////////////////////////////////////////////////////
 // STL
-#include <iostream>
+#include <sstream>
 #include <fstream>
 #include <string>
-// CppUnit
-#include <extracppunit/CppUnitCore.hpp>
+// Boost Unit Test Framework (UTF)
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE InventoryTestSuite
+#include <boost/test/unit_test.hpp>
 // StdAir
-#include <stdair/STDAIR_Service.hpp>
-#include <stdair/bom/BomManager.hpp>
-#include <stdair/bom/BomRoot.hpp>
-#include <stdair/bom/Inventory.hpp>
-#include <stdair/bom/InventoryTypes.hpp>
-#include <stdair/bom/FlightDate.hpp>
-#include <stdair/bom/FlightDateTypes.hpp>
-#include <stdair/bom/LegDate.hpp>
-#include <stdair/bom/LegDateTypes.hpp>
-#include <stdair/bom/LegCabin.hpp>
-#include <stdair/bom/LegCabinTypes.hpp>
-#include <stdair/bom/SegmentDate.hpp>
-#include <stdair/bom/SegmentDateTypes.hpp>
-#include <stdair/bom/SegmentCabin.hpp>
-#include <stdair/bom/SegmentCabinTypes.hpp>
-#include <stdair/bom/BookingClass.hpp>
-#include <stdair/bom/BookingClassTypes.hpp>
-#include <stdair/factory/FacBomManager.hpp>
+#include <stdair/basic/BasLogParams.hpp>
+#include <stdair/basic/BasDBParams.hpp>
+#include <stdair/basic/BasFileMgr.hpp>
+#include <stdair/bom/TravelSolutionStruct.hpp>
+#include <stdair/bom/BookingRequestStruct.hpp>
 #include <stdair/service/Logger.hpp>
 // AirSched
-#include <airsched/command/Simulator.hpp>
 #include <airsched/AIRSCHED_Service.hpp>
 #include <airsched/config/airsched-paths.hpp>
-// AirSched Test Suite
-#include <test/airsched/AirlineScheduleTestSuite.hpp>
 
-// //////////////////////////////////////////////////////////////////////
-void externalMemoryManagementHelper() {
+namespace boost_utf = boost::unit_test;
 
-  /**
-     The standard initialisation requires an (CSV) input file to be given.
-     The initialisation then parses that file and builds the corresponding
-     inventories.
-     <br><br>
-     So, though inventories are already built by the initialisation process,
-     another one is built from scratch, in order to test the stdair object
-     construction with a fine granularity.
-  */
+// (Boost) Unit Test XML Report
+std::ofstream utfReportStream ("AirlineScheduleTestSuite_utfresults.xml");
+
+/**
+ * Configuration for the Boost Unit Test Framework (UTF)
+ */
+struct UnitTestConfig {
+  /** Constructor. */
+  UnitTestConfig() {
+    boost_utf::unit_test_log.set_stream (utfReportStream);
+    boost_utf::unit_test_log.set_format (boost_utf::XML);
+    boost_utf::unit_test_log.set_threshold_level (boost_utf::log_test_units);
+    //boost_utf::unit_test_log.set_threshold_level (boost_utf::log_successful_tests);
+  }
+
+  /** Destructor. */
+  ~UnitTestConfig() {
+  }
+};
+
+
+// /////////////// Main: Unit Test Suite //////////////
+
+// Set the UTF configuration (re-direct the output to a specific file)
+BOOST_GLOBAL_FIXTURE (UnitTestConfig);
+
+// Start the test suite
+BOOST_AUTO_TEST_SUITE (master_test_suite)
+
+/**
+ * Test a simple inventory sell
+ */
+BOOST_AUTO_TEST_CASE (airsched_simple_inventory_sell) {
+
+  // Input file name
+  const stdair::Filename_T lScheduleInputFilename (STDAIR_SAMPLE_DIR
+                                                   "/schedule03.csv");
+
+  // Output log File
+  const stdair::Filename_T lLogFilename ("AirlineScheduleTestSuite.log");
+
+  // Check that the file path given as input corresponds to an actual file
+  bool doesExistAndIsReadable =
+    stdair::BasFileMgr::doesExistAndIsReadable (lScheduleInputFilename);
+  BOOST_CHECK_MESSAGE (doesExistAndIsReadable == true,
+                       "The '" << lScheduleInputFilename
+                       << "' input file can not be open and read");
+
+  // Set the log parameters
+  std::ofstream logOutputFile;
+  // Open and clean the log outputfile
+  logOutputFile.open (lLogFilename.c_str());
+  logOutputFile.clear();
+
+  const stdair::BasLogParams lLogParams (stdair::LOG::DEBUG, logOutputFile);
+  AIRSCHED::AIRSCHED_Service airschedService (lLogParams,
+                                              lScheduleInputFilename);
+
+  // Create an empty booking request structure
+  // TODO: fill the booking request structure from the input parameters
+  const stdair::AirportCode_T lOrigin ("NCE");
+  const stdair::AirportCode_T lDestination ("BKK");
+  const stdair::AirportCode_T lPOS ("NCE");
+  const stdair::Date_T lPreferredDepartureDate(2007, boost::gregorian::Apr, 21);
+  const stdair::Date_T lRequestDate (2007, boost::gregorian::Mar, 21);
+  const stdair::Duration_T lRequestTime (boost::posix_time::hours(8));
+  const stdair::DateTime_T lRequestDateTime (lRequestDate, lRequestTime);
+  const stdair::CabinCode_T lPreferredCabin ("Bus");
+  const stdair::PartySize_T lPartySize (3);
+  const stdair::ChannelLabel_T lChannel ("DF");
+  const stdair::TripType_T lTripType ("RO");
+  const stdair::DayDuration_T lStayDuration (5);
+  const stdair::FrequentFlyer_T lFrequentFlyerType ("NONE");
+  const stdair::Duration_T lPreferredDepartureTime (boost::posix_time::hours(10));
+  const stdair::WTP_T lWTP (2000.0);
+  const stdair::PriceValue_T lValueOfTime (20.0);
+  const stdair::BookingRequestStruct lBookingRequest (lOrigin, lDestination,
+                                                      lPOS,
+                                                      lPreferredDepartureDate,
+                                                      lRequestDateTime,
+                                                      lPreferredCabin,
+                                                      lPartySize, lChannel,
+                                                      lTripType, lStayDuration,
+                                                      lFrequentFlyerType,
+                                                      lPreferredDepartureTime,
+                                                      lWTP, lValueOfTime);
+    
+  //
+  stdair::SegmentPathList_T lSegmentPathList;
+  airschedService.buildSegmentPathList (lSegmentPathList, lBookingRequest);
+  const unsigned int lNbOfTravelSolutions = lSegmentPathList.size();
   
-  try {
-    
-    // Input file name
-    const std::string lInputFilename (STDAIR_SAMPLE_DIR "/schedule02.csv");
-    
-    // Output log File
-    const std::string lLogFilename ("AirlineScheduleTestSuite.log");
-    
-    // Set the log parameters
-    std::ofstream logOutputFile;
-    // open and clean the log outputfile
-    logOutputFile.open (lLogFilename.c_str());
-    logOutputFile.clear();
+  // TODO: change the expected number of travel solutions to the actual number
+  const unsigned int lExpectedNbOfTravelSolutions = 4;
+  
+  // DEBUG
+  STDAIR_LOG_DEBUG ("Number of travel solutions for the booking request '"
+                    << lBookingRequest.describe() << "': "
+                    << lNbOfTravelSolutions << ". It is expected to be "
+                    << lExpectedNbOfTravelSolutions << ".");
 
-    const stdair::BasLogParams lLogParams (stdair::LOG::DEBUG, logOutputFile);
-    AIRSCHED::AIRSCHED_Service airschedService (lLogParams, lInputFilename);
+  BOOST_CHECK_EQUAL (lNbOfTravelSolutions, lExpectedNbOfTravelSolutions);
 
-    // DEBUG
-    STDAIR_LOG_DEBUG ("Welcome to Air-Schedule");
-    
-    // Close the Log outputFile
-    logOutputFile.close();
+  BOOST_CHECK_MESSAGE(lNbOfTravelSolutions == lExpectedNbOfTravelSolutions,
+                      "The number of travel solutions for the booking request '"
+                      << lBookingRequest.describe() << "' is equal to "
+                      << lNbOfTravelSolutions << ", but it should be equal to "
+                      << lExpectedNbOfTravelSolutions);
 
-    // Clean the memory.
-//     stdair::FacSupervisor::instance().cleanBomContentLayer();
-//     stdair::FacSupervisor::instance().cleanBomStructureLayer();
-//     AIRSCHED::FacSupervisor::instance().cleanServiceLayer();
-//     AIRSCHED::FacSupervisor::instance().cleanLoggerService();
-
-  } catch (const std::exception& stde) {
-    std::cerr << "Standard exception: " << stde.what() << std::endl;
-    
-  } catch (...) {
-    std::cerr << "Unknown exception" << std::endl;
-  }
+  // Close the Log outputFile
+  logOutputFile.close();
 }
 
-// //////////////////////////////////////////////////////////////////////
-void AirlineScheduleTestSuite::externalMemoryManagement() {
-  CPPUNIT_ASSERT_NO_THROW (externalMemoryManagementHelper(););
-}
+// End the test suite
+BOOST_AUTO_TEST_SUITE_END()
 
-// //////////////////////////////////////////////////////////////////////
-void scheduleParsingHelper() {
+/*!
+ * \endcode
+ */
 
-  try {
-    
-    // Input file name
-    stdair::Filename_T lInputFilename (STDAIR_SAMPLE_DIR "/schedule02.csv");
-
-    // Output log File
-    std::string lLogFilename ("AirlineScheduleTestSuite.log");
-
-    // Set the log parameters
-    std::ofstream logOutputFile;
-    // open and clean the log outputfile
-    logOutputFile.open (lLogFilename.c_str());
-    logOutputFile.clear();
-
-    const stdair::BasLogParams lLogParams (stdair::LOG::DEBUG, logOutputFile);
-    AIRSCHED::AIRSCHED_Service airschedService (lLogParams, lInputFilename);
-
-    // DEBUG
-    STDAIR_LOG_DEBUG ("Schedule Parsing Test");
-    
-    // Start a mini-simulation
-    airschedService.simulate();
-
-    // Close the Log outputFile
-    logOutputFile.close();
-
-  } catch (const std::exception& stde) {
-    std::cerr << "Standard exception: " << stde.what()  << std::endl;
-    
-  } catch (...) {
-    std::cerr << "Unknown exception" << std::endl;
-  }
-}
-
-// //////////////////////////////////////////////////////////////////////
-void AirlineScheduleTestSuite::scheduleParsing() {
-  CPPUNIT_ASSERT_NO_THROW (scheduleParsingHelper(););
-}
-
-// //////////////////////////////////////////////////////////////////////
-AirlineScheduleTestSuite::AirlineScheduleTestSuite () {
-  _describeKey << "Running test on AIRSCHED Optimisation function";  
-}
-
-// /////////////// M A I N /////////////////
-CPPUNIT_MAIN()
---
+++ test/airsched/sources.mk
@@ -0,0 +1,6 @@
+airsched_test_lib_h_sources =
+airsched_test_lib_cc_sources =
+
+airsched_test_bin_h_sources =
+airsched_test_bin_cc_sources = \
+	$(top_srcdir)/test/airsched/AirlineScheduleTestSuite.cpp
