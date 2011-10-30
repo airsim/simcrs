9a5edf518b8c71183c9dd0fa98afa61fc89df8a1
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-10-30 23:37:28 +0100

    [Build] Moved AirSched, AirRAC, RMOL, AirInv and SimFQT out of the (SimCRS) project, as they now are stand-alone components.

--- test/airsched/sources.mk
+++
@@ -1,6 +0,0 @@
-airsched_test_lib_h_sources =
-airsched_test_lib_cc_sources =
-
-airsched_test_bin_h_sources =
-airsched_test_bin_cc_sources = \
-	$(top_srcdir)/test/airsched/AirlineScheduleTestSuite.cpp
