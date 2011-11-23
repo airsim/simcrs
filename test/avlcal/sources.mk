8c2e6a9f6d101681276cc465a46a38fd79b73c90
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-11-23 19:03:15 +0100

    [Struct] Removed the embedded AvlCal and SimLFS components.

--- test/avlcal/sources.mk
+++
@@ -1,6 +0,0 @@
-avlcal_test_lib_h_sources =
-avlcal_test_lib_cc_sources =
-
-avlcal_test_bin_h_sources =
-avlcal_test_bin_cc_sources = \
-	$(top_srcdir)/test/avlcal/AvlCalTestSuite.cpp
--- test/simlfs/sources.mk
+++
@@ -1,6 +0,0 @@
-simlfs_test_lib_h_sources =
-simlfs_test_lib_cc_sources =
-
-simlfs_test_bin_h_sources =
-simlfs_test_bin_cc_sources = \
-	$(top_srcdir)/test/simlfs/LFSTestSuite.cpp
